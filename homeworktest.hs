43dd000e6211070c32c70c1993907c4c327af25c
GZQ <765194873@qq.com>
2018-09-20 22:54:59 +0800

    添加了gitignore文件

--- homeworktest.hs
+++ homeworktest.hs
@@ -1,11 +1,15 @@
-f0 :: Int -> Bool
-f0 x = _ || _
 
-f1 :: Int -> Int 
-f1 x = x + 1
+f1 :: Int -> Bool 
+f1 x = b || c
+   | b==c  = c
+   | otherwise = True
+ 
 
 f2 :: (Int -> Int) -> Int
 f2 x = x 2
 
 pair :: Num a => Num b => a -> b -> (a, b)
 pair a b = (a, b)
+
+halve xs = (take (n `div` 2) xs, drop (n `div` 2) xs)
+    where n = length xs 
