30fb7528ee8324329718d37a1b171dc1f6f74a57
GZQ <765194873@qq.com>
2018-10-10 19:33:02 +0800

    triands 函数

---
+++ LibExample.hs
@@ -0,0 +1,18 @@
+type Person = String
+type Book = String
+type DataBase = [(Person, Book)]
+
+makeLoan :: DataBase -> Person -> Book -> DataBase
+makeLoan db person book = (person, book) : db
+
+returnLoan :: DataBase -> Person -> Book -> DataBase
+returnLoan db person book = [(p, b) | (p, b) <- db, (p, b) /= (person, book)]
+
+books :: DataBase -> Person -> [Book]
+books db person = [b | (p, b) <- db, person == p ]
+
+numBorrowed :: DataBase -> Person -> Int
+numBorrowed db person = length (books db person)
+
+borrowers :: DataBase -> Book -> [Person]
+borrowers db book = [p | (p, b) <- db, b == book]
